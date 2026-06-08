a939eac754b2d4c77bf43b8391f2d4a1d0bacbb3
helloreunified <r3un1t3d.wav3g@gmail.com>
2026-06-08 10:46:21 +0700

    version 3 (highly unstable)

--- main.cpp
+++ main.cpp
@@ -7,11 +7,12 @@
 #include <sstream>
 #include <filesystem>
 #include <fstream>
+#include <sys/wait.h>
 
 std::string shutils[] = {"exit", "cd", "help", "pwd", "exec", "type", "export", "unset"};
 struct {
 	std::string home, user, hostname;
-	std::unordered_map<std::string, std::string> alias;
+	std::unordered_map<std::string, std::string> aliastable;
 } shellenv; // contain shell-specific environment tables
 
 void fetchenv()
@@ -65,8 +66,12 @@ std::string foundexe(std::string file)
 int shellcmd(const std::vector<std::string>& tokens)
 {
 	if (tokens[0]=="unalias") {
-		std::cerr << "Unavailable.\n";
-		return 1;
+		if (tokens[1].size()>2) std::cerr << "Why 2 or more parameters?\n";
+		if (!shellenv.aliastable.erase(tokens[1])) {
+			std::cerr << "Alias not found.\n";
+			return 1;
+		}
+		return 0;
 	}
 
 	if (tokens[0]=="cd") {
@@ -127,7 +132,7 @@ int shellcmd(const std::vector<std::string>& tokens)
 			if (tokens.size()>2) std::cerr << "Probably too much parameters, ignoring\n";
 			std::string fetch = tokens[1];
 	
-			if (shellenv.alias.find(fetch)!=shellenv.alias.end()) {
+			if (shellenv.aliastable.find(fetch)!=shellenv.aliastable.end()) {
 				std::cout << fetch << " is an alias.\n";
 				return 0;
 			}
@@ -178,7 +183,7 @@ int shellcmd(const std::vector<std::string>& tokens)
 			// wait so what function I'll use for this one
 		}
 
-	if (tokens[0]=="jobs") {
+	if (tokens[0]=="jobs" || tokens[0]=="fg" || tokens[0]=="bg") {
 		std::cerr << "Catastrophic error. Not really, though.\n";
 		return 7;
 	}
@@ -197,26 +202,145 @@ int shellcmd(const std::vector<std::string>& tokens)
 		}
 
 	if (tokens[0]=="alias") {
-		std::cerr << "Not available." << '\n';
-		return 9;
+		if (tokens.size()>2) std::cerr << "Ignoring unnecessary specifications.\n";
+
+		size_t delimiter = tokens[1].find("=");
+		if (delimiter==std::string::npos) {
+			std::cerr << "You're missing out on something!";
+			return 9;
+		}
+
+		std::string name = tokens[1].substr(0, delimiter);
+		std::string val = tokens[1].substr(delimiter+1);
+
+		shellenv.aliastable[name] = val;
 	}
 
 	return -1;
 }
 
-int echoutil(std::string readline)
+int echoutil(std::vector<std::string> tokens)
 {
-	return 0;
+	if (tokens.size()<2)
+		std::cout << "Example help text\n";
+	else
+		for (size_t i=1; i<tokens.size(); i++)
+			std::cout << tokens[i];
+	std::cout << '\n';
+	return 0; // will do more stuff here
+}
+
+std::pair<std::string, bool> rtvarexp(std::string name) // runtime variable expand
+{
+	const char* fetch = std::getenv(name.c_str());
+
+	if (fetch!=nullptr)
+		return {(std::string)fetch, false};
+	else
+		return {"", true}; // evaluates to true if variable not found
 }
 
 std::vector<std::string> parse(std::string readline)
 {
-	return {};
+	std::vector<std::string> tokens;
+	std::string qtbuff(""), keyword("");
+	uint16_t quotestate = 0; // quote buffer and state
+	
+	for (size_t i=0; i<readline.size(); i++)
+	{
+		char fetch = readline[i];
+
+		if (quotestate==0) {
+			if (fetch=='\"') quotestate=1;
+			else if (fetch=='\'') quotestate=2;
+			else if (fetch!=' ') keyword += fetch;
+			else {
+				tokens.push_back(keyword);
+				keyword.clear();
+			}
+		} else
+
+		if (quotestate==1) {
+			if (fetch=='\"' || fetch==' ') {
+				if (keyword[0]=='$') {
+					std::pair<std::string, bool> retrieval = rtvarexp(keyword.substr(1));
+					if (retrieval.second) {
+						std::cerr << "Error expanding variable " << keyword << ".\n";
+						return {};
+					}
+					qtbuff += retrieval.first;
+				}
+				else qtbuff += keyword;
+
+				if (fetch=='\"') {
+					quotestate=0;
+					tokens.push_back(qtbuff);
+					qtbuff.clear();
+				}	
+				else qtbuff += " ";
+
+				keyword.clear();
+			}
+			else keyword += fetch;
+		} else
+
+		if (quotestate==2) {
+			if (fetch=='\'') {
+				tokens.push_back(qtbuff);
+				qtbuff.clear();
+			}
+			else qtbuff += fetch;
+		} else
+
+		{
+			std::cerr << "Catastrophic error.\n";
+			return {};
+		}
+	}
+	if (!keyword.empty())
+		tokens.push_back(keyword);
+	if (!qtbuff.empty() || quotestate!=0) {
+		std::cerr << "Imcomplete command.\n";
+		return {};
+	};
+	
+	return tokens;
 }
 
 int cmdexec(const std::vector<std::string>& tokens)
 {
-	return system(tokens[0].c_str());
+	std::vector<char*> argvect;
+	for (const std::string& have : tokens)
+		argvect.push_back(const_cast<char*>(&have[0]));
+	argvect.push_back(nullptr);
+
+	pid_t pid = fork();
+	
+	if (pid<0) {
+		std::cerr << "Failed to create process.\n";
+		return -3;
+	}
+	if (!pid) {
+		execvp(argvect[0], argvect.data());
+
+		std::cerr << "Command not found. What did you expect from typing the executable wrong?\n";
+		_exit(127);
+	}
+	else {
+		int status = 0;
+		pid_t waitcode = waitpid(pid, &status, 0);
+		// let's wait for the child, writing exit code to status and use default behavio
+
+		if (waitcode==-1) {
+			std::cerr << "I checked down there and there's no such child process.";
+			return -2;
+		}
+
+		if (WIFEXITED(status))
+			return WEXITSTATUS(status);
+		
+		return -1; // I don't know how one can reach here, but leave it here
+	}
 }
 
 std::string expandDir(std::string dir)
@@ -247,10 +371,8 @@ bool isScript(std::string readline)
 	}
 
 	// or check file extension
-	std::string extension("");
-	for (size_t i=filepath.size()-1; filepath[i]!='.' && i>=0; i--)
-		extension = filepath[i] + extension;
-	if (extension=="sh" || extension=="py")
+	std::string extension = std::filesystem::path(filepath).extension().string();
+	if (extension==".sh" || extension==".py")
 		return true;
 
 	return false;
@@ -283,17 +405,17 @@ int main()
 		if (readline=="exit") {
 			lastexit = 0;
 			break;
-		}// else if (isScript(readline)) {
-		//	lastexit = system(expandDir(readline).c_str()); }
-		else { lastexit = system(readline.c_str());
-		//	std::vector<std::string> tokens = parse(readline);
-		//	if (tokens.empty()) continue; // typed nothing
-		//	
-		//	int opcode = shellcmd(tokens);
-		//	if (tokens[0]!="echo")
-		//		lastexit = (opcode==-1) ? cmdexec(tokens) : opcode;
-		//	else
-		//		lastexit = echoutil(readline);
+		} else if (isScript(readline)) {
+			lastexit = system(expandDir(readline).c_str()); }
+		else {
+			std::vector<std::string> tokens = parse(readline);
+			if (tokens.empty()) continue; // typed nothing
+			
+			int opcode = shellcmd(tokens);
+			if (tokens[0]!="echo")
+				lastexit = (opcode==-1) ? cmdexec(tokens) : opcode;
+			else
+				lastexit = echoutil(tokens);
 		}
 	}
 
